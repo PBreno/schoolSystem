ff1d41217ec425e7294cbe8e4480d42cd729e5a8
PBreno <brenopombobp@hotmail.com>
2024-03-28 12:31:00 -0300

    backup windows finished!

--- Front/tools/BackupWindows.py
+++ Front/tools/BackupWindows.py
@@ -1,3 +1,36 @@
+import sys
+from tkinter import ttk, messagebox
+import tkinter as tk
+from Front.PrincipalWindow import *
+
 #Backup to be implemented
 class BackupWindows:
-    pass
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Backup")
+        self.root.geometry("350x200")
+        self.root.resizable(True, True)
+
+    def backup_windows_confirmation(self):
+
+        msg= messagebox.askyesno("Backup", "Deseja fazer o backup das informações?")
+        if not msg:
+            self.root.destroy()
+            sys.exit()
+            PrincipalWindow().principalWindow()
+
+        backuplabel = tk.Label(self.root, text="Processo de Backup ")
+        backuplabel.place(x=120, y=50)
+
+        progressbar = ttk.Progressbar()
+        progressbar.place(x=75, y=70, width=200, height=25)
+        progressbar.config(maximum=100)
+
+        backButton = tk.Button(self.root, text="Cancelar")
+        backButton.place(x=120, y=150, width=100, height=25)
+        #backButton.bind("<Button-1>", )                                                                    int(creditCostEntry.get())))
+
+        self.root.mainloop()
+
+if __name__ == '__main__':
+    BackupWindows().backup_windows_confirmation()
